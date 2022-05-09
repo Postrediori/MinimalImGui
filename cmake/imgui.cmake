ac4edb413ce82783bee8aa5e8e86b9cabe9cfb14
Rasim Labibov <postrediori@gmail.com>
2022-05-09 20:45:34 +0300

    Use git submodule for ImGui

--- cmake/imgui.cmake
+++ cmake/imgui.cmake
@@ -1,24 +1,35 @@
 set(IMGUI_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/3rdparty/imgui)
 file(GLOB IMGUI_SOURCES ${IMGUI_INCLUDE_DIR}/*.cpp)
 file(GLOB IMGUI_HEADERS ${IMGUI_INCLUDE_DIR}/*.h)
-                 
-add_library(imgui STATIC ${IMGUI_SOURCES} ${IMGUI_SOURCES})
 
-add_definitions(-DIMGUI_IMPL_OPENGL_LOADER_GLAD)
+set(IMGUI_BACKENDS_DIR ${IMGUI_INCLUDE_DIR}/examples)
+set(IMGUI_BACKEND_SOURCES
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_glfw.cpp
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl2.cpp
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl3.cpp)
+set(IMGUI_BACKEND_HEADERS
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_glfw.h
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl2.h
+    ${IMGUI_BACKENDS_DIR}/imgui_impl_opengl3.h)
 
-include_directories(
+add_library(imgui STATIC
+    ${IMGUI_SOURCES} ${IMGUI_SOURCES}
+    ${IMGUI_BACKEND_HEADERS} ${IMGUI_BACKEND_SOURCES})
+
+target_compile_definitions(imgui PUBLIC IMGUI_IMPL_OPENGL_LOADER_GLAD)
+
+target_include_directories(imgui PUBLIC
     ${IMGUI_INCLUDE_DIR}
+    ${IMGUI_BACKENDS_DIR}
     ${OPENGL_INCLUDE_DIR}
     ${GLFW_INCLUDE_DIR}
-    ${GLAD_INCLUDE_DIR}
-    )
-    
+    ${GLAD_INCLUDE_DIR})
+
 target_link_libraries(imgui
     ${OPENGL_LIBRARIES}
     ${GLFW_LIBRARIES}
-    ${GLAD_LIBRARIES}
-    )
-    
+    ${GLAD_LIBRARIES})
+
 set_target_properties(imgui PROPERTIES LINKER_LANGUAGE CXX)
 set_target_properties(imgui PROPERTIES FOLDER 3rdparty)
 
